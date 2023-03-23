fbff9c1121b45ddc00757b29b07f42ae6b9f9e9d
vuustrdr <vdsatiroglud@gmail.com>
2023-03-23 22:09:46 +0000

    nearly there

--- BLE_Initial.ino
+++ BLE_Initial.ino
@@ -2,26 +2,18 @@
 #include <HTTPClient.h>
 #include <WiFi.h>
 #include <unordered_set>
-#include <esp_system.h>
-#include <EEPROM.h>
-#include <ArduinoJson.h>
-#include <cstring>
 #include "scannerVars.h"
 #include "helperFunctions.h"
 
 // Scan every 10 seconds and send a post request to the configured endpoint.
-// When the wifi fails to connect (i.e the password has been changed), send a get request asking for the updated creds. 
-// If they are different restart the machine with new creds
 
 void setup() {
   Serial.begin(115200);
-  EEPROM.begin(EEPROM.length());
 
   Serial.println("Initializing...");
 
-  setWifiCreds();
-
   BLEDevice::init("");
+  // BLEDevice::setPower(ESP_PWR_LVL_P9);
   WiFi.begin(ssid, pass);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -34,58 +26,12 @@ void setup() {
 
 void loop() {
 
-  // Send the device count to the server
   if (WiFi.status() == WL_CONNECTED) {
 
     scanAndSend();
 
-  } else { 
-    //if wifi disconnects during loop, assume wifi pass or ssid has changed. Get from server.
-    HTTPClient http;
-    int response = http.begin("http://192.168.1.102:3001/requests/test_3/connect");
-
-    if (response == HTTP_CODE_OK) {
-      
-      String response = http.getString();
-      StaticJsonDocument<200> jsonDoc;
-      deserializeJson(jsonDoc, response);
-      const char* serverSsid = jsonDoc["ssid"];
-      const char* serverPass = jsonDoc["pass"];
-
-      if (serverSsid != ssid) {
-
-        for (int i = 0; i < ssidMaxLength; i++) {
-          EEPROM.write(addr + i, ssid[i]);
-        }
-
-        if (initial == 1) {
-          EEPROM.write(addr_3, 0);
-        } //write to 0
-
-        EEPROM.commit();
-        esp_restart();
-
-      } 
-      
-      if (serverPass != pass) {
-
-        for (int i = 0; i < passMaxLength; i++) {
-          EEPROM.write(addr_2 + i, pass[i]);
-        }   
-
-        if (initial == 1) {
-          EEPROM.write(addr_3, 0);
-        } //write to 0
-
-        EEPROM.commit();  
-        esp_restart();
-
-      } 
-
-    }
-    
   }
-  // Wait 10 seconds before scanning again
+
   delay(10000);
 }
 
--- scannerVars.h
+++ scannerVars.h
@@ -1,12 +1,4 @@
-const int ssidMaxLength = 32;
-const int passMaxLength = 63;
+char ssid[] = "2539 Hyperoptic 1Gbps Broadband";
+char pass[] = "a77yeyk6";
+String endpoint = "http://192.168.1.102:3001/requests/test_3";
 
-//initial connectivity keys and values
-char ssid[ssidMaxLength + 1] = "2539 Hyperoptic 1Gbps Broadband";
-char pass[passMaxLength + 1] = "a77yeyk6";
-int initial = 1; //true
-
-//addresses to hold information
-const int addr = 0;
-const int addr_2 = 100;
-const int addr_3 = 200;
